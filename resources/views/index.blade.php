5ce7f386052016f7003000e294855d532ca29758
Andre Karrlein <andre-karrlein@users.noreply.github.com>
2017-10-08 19:00:21 +0200

    update content

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -53,7 +53,7 @@
           <div class="row">
             <div class="col-lg-8 mx-auto">
               <h1 class="brand-heading">André Karrlein</h1>
-              <p class="intro-text">Software Engineer & Lead Developer</p>
+              <p class="intro-text">Software Engineer</p>
               <a href="#about" class="btn btn-circle js-scroll-trigger">
                 <i class="fa fa-angle-double-down animated"></i>
               </a>
