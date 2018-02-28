77c85afebed144a8f754764661e40da81f8e2906
Andre Karrlein <andre.karrlein@karrlein.com>
2018-02-28 07:32:01 +0000

    add resume route

--- public/index.php
+++ public/index.php
@@ -4,12 +4,15 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = new Slim\App();
 $app->get('/', function($request, $response, $args) {
+    return $response->withRedirect('/resume');
+});
+$app->get('/resume', function($request, $response, $args) {
 	$factory = new ak1\karrlein\Factory();
 	return $response->write($factory->createMainPage()->getTemplate());
 });
 $app->get('/old', function($request, $response, $args) {
     $factory = new ak1\karrlein\Factory();
     return $response->write($factory->createMainPageOld()->getTemplate());
-}); 
+});
 
 $app->run();
