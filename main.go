bd8b5cd451285002c323a71051231b1636e82fe9
André Karrlein <andre.karrlein@karrlein.com>
2021-02-05 19:10:39 +0000

    move to wasm init

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/maxence-charriere/go-app/v7/pkg/app"
+)
+
+// The main function is the entry of the server. It is where the HTTP handler
+// that serves the UI is defined and where the server is started.
+//
+// Note that because main.go and app.go are built for different architectures,
+// this main() function is not in conflict with the one in
+// app.go.
+func main() {
+	// app.Handler is a standard HTTP handler that serves the UI and its
+	// resources to make it work in a web browser.
+	//
+	// It implements the http.Handler interface so it can seamlessly be used
+	// with the Go HTTP standard library.
+	http.Handle("/", &app.Handler{
+		Name:        "Karrlein.com",
+		Title:       "Karrlein.com",
+		Description: "Home of André Karrlein",
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
